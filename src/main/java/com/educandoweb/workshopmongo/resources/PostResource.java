cb7227240e58adec001eec98bafb2924e325e5b8
Antony Rafael <antonyrafaelsouzaa@gmail.com>
2024-11-04 12:53:10 -0300

    Consulta simples com Query Methods

--- src/main/java/com/educandoweb/workshopmongo/resources/PostResource.java
+++ src/main/java/com/educandoweb/workshopmongo/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.educandoweb.workshopmongo.resources;
  
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.workshopmongo.domain.Post;
+import com.educandoweb.workshopmongo.resources.util.URL;
 import com.educandoweb.workshopmongo.services.PostService;
 
 @RestController
@@ -22,5 +26,11 @@ public class PostResource {
 		Post post = service.findById(id);
 		return ResponseEntity.ok().body(post);
 	}
-
+	
+	@GetMapping("/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(defaultValue="") String text) {
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
 }
